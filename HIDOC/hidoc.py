7a25d80c781ffa7430b642f2dd8c5535ac1c47d8
taehopark0! <taehojj@gmail.com>
2021-07-23 00:09:17 +0900

    하이닥 스크래핑 업데이트
    - 질문에 다수 답변의 Case 수정
    - 케이스 구분
    - 셀레니움을 활용한 page click
    & back 프로세스

--- HIDOC/hidoc.py
+++ HIDOC/hidoc.py
@@ -35,35 +35,56 @@ lst_answers=[]
 
 URL = "https://www.hidoc.co.kr/healthqna/list?page="
 
-for i in range(1,3):
+for i in range(1,10):
     #Selenium용 Parsing
     URL_page = URL+str(i)
     driver.get(URL_page)
     html = driver.page_source
     bs = bs4.BeautifulSoup(html, "html.parser")
     elems = driver.find_elements_by_class_name("desc")
-    for x in range(1,len(elems)+1):
-        #stale error
-        time.sleep(10)
+    print(len(elems))
+    for x in range(1,len(elems)):
+        elems=driver.find_elements_by_class_name("desc")
         elems[x].click()
         url_requests = driver.page_source
         url_soup = BeautifulSoup(url_requests,'html.parser')
         titles=url_soup.select_one("strong.tit").text
         question_time = url_soup.select_one("span.txt_time").text
         questions = url_soup.select('div[class="box_type1 view_question"]>div[class="inner"]>div[class="desc"]>p')[0].get_text()
-        name_docs = url_soup.select_one("a.link_doctor").text
-        name_hospitals = url_soup.select_one("span.txt_clinic").text
-        answers = url_soup.select('div[class="answer_body"]>div[class="cont"]>div[class="desc"]')[0].get_text()
+        if titles == "당뇨 망막증":
+            answers = url_soup.select(
+                'div[class="box_type1 hidoc_answer"]>div[class="answer_body"]>div[class="cont"]>div[class="desc"]')[
+                0].get_text()
+            name_hospitals = url_soup.find("span", attrs={"class": "txt_clinic"})
+            if type(name_hospitals) == bs4.element.Tag:
+                name_hospitals = name_hospitals.text
+            else:
+                name_hospitals = '0'
+        elif titles in lst_titles:
+            answers = url_soup.select('div[class="box_type1 hidoc_answer"]>div[class="answer_body"]>div[class="cont"]>div[class="desc"]')[2].get_text()
+            name_hospitals = url_soup.select('span[class= "txt_clinic"]')[1]
+            if type(name_hospitals) == bs4.element.Tag:
+                name_hospitals = name_hospitals.get_text()
+            else:
+                name_hospitals = '0'
+        elif titles not in lst_titles:
+            answers = url_soup.select('div[class="box_type1 hidoc_answer"]>div[class="answer_body"]>div[class="cont"]>div[class="desc"]')[0].get_text()
+            name_hospitals = url_soup.find("span", attrs={"class": "txt_clinic"})
+            if type(name_hospitals) == bs4.element.Tag:
+                name_hospitals = name_hospitals.text
+            else:
+                name_hospitals = '0'
+
         lst_titles.append(titles)
         lst_question_time.append(question_time)
         lst_questions.append(questions)
-        lst_name_docs.append(name_docs)
         lst_name_hospitals.append(name_hospitals)
         lst_answers.append(answers)
         driver.back()
 
     print(i)
-df = pd.DataFrame(zip(lst_titles,lst_question_time,lst_questions,lst_name_docs,lst_name_hospitals,lst_answers))
-df.rename(columns={0:'제목',1:'질문 시간',2:'질문',3:'의사 이름', 4:'병원 이름', 5:'답변'},inplace=True)
+df = pd.DataFrame(zip(lst_titles,lst_question_time,lst_questions,lst_name_hospitals,lst_answers))
+df.rename(columns={0:'제목',1:'질문 시간',2:'질문',3:'병원 이름', 4:'답변'},inplace=True)
 df.to_excel("hidoc.xlsx",index=False)
 print(df)
+driver.quit()
